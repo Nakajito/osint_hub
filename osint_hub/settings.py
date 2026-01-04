57c9795731d1de033d4c3c814babc4681df87192
Daniel <nakajito@proton.me>
2026-01-04 16:22:49 -0500

    fix(database): wrong db

--- osint_hub/settings.py
+++ osint_hub/settings.py
@@ -70,12 +70,13 @@ WSGI_APPLICATION = "osint_hub.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# settings.py
+
 DATABASES = {
-    "default": config(
-        "DATABASE_URL",
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        cast=dj_database_url.parse,
-    )
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 
